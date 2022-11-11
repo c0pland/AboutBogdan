e032f6245f5f5219eb289856ee5cf491bb9a79bc
Bogdan Benner <c0pland@icloud.com>
2022-11-11 17:24:52 +0200

    Initial Commit

---
+++ AboutBogdan/AboutBogdanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutBogdanApp.swift
+//  AboutBogdan
+//
+//  Created by Богдан Беннер on 11.11.22.
+//
+
+import SwiftUI
+
+@main
+struct AboutBogdanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
